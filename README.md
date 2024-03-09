ee47f7fe555c5864164677bbc472de566f4eb6db
woosolkim <woosol2da@gmail.com>
2024-03-10 00:21:47 +0900

    add react-router-dom

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ### Studying for Bundlers & Compilers
+
+1. ESBuild, SWC
+2. React
+3. ESBuild -> vite Migration (Upcoming)
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import ReactDOMClient from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import App from "./app";
 
-document.body.innerHTML = '<div id="app"></div>';
-const container = document.getElementById("root") as HTMLElement;
-const root = ReactDOMClient.createRoot(container);
-
-root.render(<h1>Hello, world</h1>);
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
